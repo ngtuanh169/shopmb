41b04d68ce2c99d2f80b72e8ed7ab531b190a014
ngtuanh169 <117904160+ngtuanh169@users.noreply.github.com>
2022-11-12 12:40:08 +0700

    Update index.js

--- src/pages/Admin/Components/Modal/index.js
+++ src/pages/Admin/Components/Modal/index.js
@@ -2,36 +2,34 @@ import FadeIn from '../../../../components/FadeIn';
 import Button from '../../../../components/Button';
 import style from './Modal.module.css';
 
-function Modal({ title, type, children, onSubmit, onClick,closeForm }) {
+function Modal({ title, type, children, onSubmit, onClick }) {
     return (
-           <>
-            <FadeIn onClick={closeForm}>
-            </FadeIn>
-            <div className={style.wapper}>
-                <div className={style.row}>
-                    <div className={style.close}>
-                        <Button small transparent onClick={onClick}>
-                            <i className="fa-solid fa-xmark"></i>
-                        </Button>
-                    </div>
-                    <div className={style.title}>
-                        <span>{title}</span>
-                    </div>
-                    <div className={style.btn}>
-                        {type === 'add' ? (
-                            <Button blueAdmin icon={'fa-solid fa-plus'} onClick={onSubmit}>
-                                Thêm mới
+            <FadeIn >
+                <div className={style.wapper}>
+                    <div className={style.row}>
+                        <div className={style.close}>
+                            <Button small transparent onClick={onClick}>
+                                <i className="fa-solid fa-xmark"></i>
                             </Button>
-                        ) : (
-                            <Button blueAdmin icon={'fa-solid fa-pen-to-square'} onClick={onSubmit}>
-                                Sửa
-                            </Button>
-                        )}
+                        </div>
+                        <div className={style.title}>
+                            <span>{title}</span>
+                        </div>
+                        <div className={style.btn}>
+                            {type === 'add' ? (
+                                <Button blueAdmin icon={'fa-solid fa-plus'} onClick={onSubmit}>
+                                    Thêm mới
+                                </Button>
+                            ) : (
+                                <Button blueAdmin icon={'fa-solid fa-pen-to-square'} onClick={onSubmit}>
+                                    Sửa
+                                </Button>
+                            )}
+                        </div>
+                        <div className={`${style.formContent} custom-scrollbars`}>{children}</div>
                     </div>
-                    <div className={`${style.formContent} custom-scrollbars`}>{children}</div>
                 </div>
-            </div>
-           </>
+           </FadeIn>
     );
 }
 
