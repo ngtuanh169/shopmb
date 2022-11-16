aa3fe6bb6b56bcdc2771a0cffff1196d8b41ad5a
ngtuanh169 <117904160+ngtuanh169@users.noreply.github.com>
2022-11-16 19:29:41 +0700

    Update index.js

--- src/pages/Product/components/Comments/CommentItem/index.js
+++ src/pages/Product/components/Comments/CommentItem/index.js
@@ -29,14 +29,14 @@ function CommentItem({ data }) {
             const newArr = arrLikeCmt.filter((item) => {
                 return item === data.id;
             });
-            setLike(newArr.length > 0);
+            newArr && setLike(newArr.length > 0);
         }
         if (user.dislikeComments) {
             const arrDislikeCmt = JSON.parse(user.dislikeComments);
             const newArr = arrDislikeCmt.filter((item) => {
                 return item === data.id;
             });
-            setDislike(newArr.length > 0);
+            newArr && setDislike(newArr.length > 0);
         }
     }, []);
     const handleSubmit = async () => {
